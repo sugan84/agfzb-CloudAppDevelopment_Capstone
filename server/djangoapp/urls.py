8515af5b69121d2c63e0df068674f0fa277fe21b
sugan84 <sugandran@gmail.com>
2021-11-29 16:55:07 +0000

    adding temporary changes to github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,18 +8,9 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,32 +18,25 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    return render(request, 'djangoapp/about.html')
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
-
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
