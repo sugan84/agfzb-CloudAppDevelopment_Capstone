095ae83887966e9bcc2572e8b898a2e9e16b97de
sugan84 <sugandran@gmail.com>
2021-11-30 11:33:21 +0000

    Second part of the assignment done - user admin

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
     path(route='about', view=views.about, name='about'),
     path(route='contact', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
